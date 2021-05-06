427149efadf369afcba50de31c397d20ef80fd82
Gokul <>
2021-05-06 16:46:35 +0530

    Comments removed

--- database/database.go
+++ database/database.go
@@ -2,13 +2,10 @@ package database
 
 import (
 	"fmt"
-	"log"
 )
 
-// Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	log.Println("Called Hellow")
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
